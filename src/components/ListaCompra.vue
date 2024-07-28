24bc1a3def9e0ede31c8d731739e5beddb70b3e6
Caio H. Rodrigues <118766140+caiohenri-R@users.noreply.github.com>
2024-07-28 15:49:56 -0300

    Projeto Lista

---
+++ src/components/ListaCompra.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <div class="container">
+        <h1>Lista de Compras</h1>
+        <hr />
+        <p><input type="text" v-model="produto" placeholder="Nome do Produto" class="form-control"></p>
+        <p><input type="text" v-model="quantidade" placeholder="Quantidade do Produto" class="form-control"></p>
+        <button type="submit" v-on:click.prevent="incluirProduto" class="btn btn-primary">Incluir</button>
+        <hr/>
+        <div class="list-group">
+            <div class="list-group-item" v-for="(item, index) in lista" v-bind:key="index">
+                <span><strong>{{item.produto}}</strong></span>
+                <p>{{item.quantidade}}</p>
+                <div>
+                    <a href="#">Excluir</a>   
+                </div>
+            </div>              
+       </div>
+    </div>
+</template>
+
+<script>
+export default{
+   data(){
+    return {
+            produto: '',
+            quantidade:'',
+            lista: []
+        
+        }
+     },
+     methods:{
+         incluirProduto(){
+            console.log(this.produto , this.quantidade);
+            this.lista.push({
+                produto:this.produto,
+                quantidade:this.quantidade 
+            });
+            this.produto = '';
+            this.quantidade = '';
+            
+         }
+     }
+}
+</script>
---
+++ src/views/ListaComprasView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ListaCompra from '../components/ListaCompra.vue'
+</script>
+
+<template>
+<main>
+  <ListaCompra />
+</main>
+</template>
+
+
